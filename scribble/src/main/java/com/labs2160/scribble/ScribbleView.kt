360bb3a8c1f96b6e08650269906fdcfc390acfc0
hsl43 <homer.leonard@outlook.com>
2016-08-25 12:32:06 +0900

    drawing to bitmap

--- scribble/src/main/java/com/labs2160/scribble/ScribbleView.kt
+++ scribble/src/main/java/com/labs2160/scribble/ScribbleView.kt
@@ -1,10 +1,7 @@
 package com.labs2160.scribble
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.support.v4.view.MotionEventCompat
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -14,7 +11,12 @@ class ScribbleView : View {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    private val strokePaths = Path()
+    companion object {
+        private val TOUCH_TOLERANCE = 4
+        private val STROKE_WIDTH = 6F
+    }
+
+    private val path = Path()
 
     private val paint = Paint().apply {
         isAntiAlias = true
@@ -23,18 +25,27 @@ class ScribbleView : View {
         style       = Paint.Style.STROKE
         strokeJoin  = Paint.Join.ROUND
         strokeCap   = Paint.Cap.ROUND
-        strokeWidth = 6F
+        strokeWidth = STROKE_WIDTH
     }
+
+    private lateinit var canvasBitmap: Bitmap
+    private lateinit var canvas: Canvas
 
     private var lastX = 0F
     private var lastY = 0F
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas       = Canvas(canvasBitmap)
+    }
+
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        canvas.drawPath(strokePaths, paint)
-
-        invalidate()
+        with(canvas) {
+            drawBitmap(canvasBitmap, 0F, 0F, paint)
+            drawPath(path, paint)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
@@ -51,30 +62,45 @@ class ScribbleView : View {
 
         when(MotionEventCompat.getActionMasked(event)) {
             MotionEvent.ACTION_DOWN -> {
-                strokePaths.moveTo(currentX, currentY)
+                path.reset()
+                path.moveTo(currentX, currentY)
 
                 lastX = currentX
                 lastY = currentY
             }
 
             MotionEvent.ACTION_MOVE -> {
-                strokePaths.quadTo(lastX, lastY, (currentX + lastX) / 2, (currentY + lastY) / 2)
+                val dx = Math.abs(currentX - lastX)
+                val dy = Math.abs(currentY - lastY)
 
-                lastX = currentX
-                lastY = currentY
+                if(dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                    path.quadTo(lastX, lastY, (currentX + lastX) / 2, (currentY + lastY) / 2)
+
+                    lastX = currentX
+                    lastY = currentY
+                }
             }
 
-            MotionEvent.ACTION_UP      -> { /* unimplemented */ }
+            MotionEvent.ACTION_UP -> {
+                path.lineTo(lastX, lastY)
+
+                canvas.drawPath(path, paint)
+
+                path.reset()
+            }
+
             MotionEvent.ACTION_CANCEL  -> { /* unimplemented */ }
             MotionEvent.ACTION_OUTSIDE -> { /* unimplemented */ }
         }
+
+        invalidate()
 
         return true
     }
 
     fun clear() {
-        if(!strokePaths.isEmpty) {
-            strokePaths.reset()
+        if(!path.isEmpty) {
+            path.reset()
             invalidate()
         }
     }
